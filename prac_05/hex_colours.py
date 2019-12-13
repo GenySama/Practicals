dfe2fa8b52a1e6107481168c34b9282bc72ab19f
GenySama <geenysong@gmail.com>
2019-12-13 19:42:32 +0800

    add branches

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,7 +3,8 @@ COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
                 "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
                 "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
                 "aquamarine4": "#458b74", "azure1": "#f0ffff"}
-colour_name = input("Please enter a colour name: ")
+COLOUR_CODES = {key.lower(): value for key, value in COLOUR_CODES.items()}
+colour_name = input("Please enter a colour name: ").lower()
 while colour_name != "":
-    print("The code for {} is {}".format(colour_name,COLOUR_CODES.get(colour_name)))
-    colour_name = input("Please enter a colour name: ")
+    print("The code for {} is {}".format(colour_name, COLOUR_CODES.get(colour_name)))
+    colour_name = input("Please enter a colour name: ").lower()
