334a44588a5f3efbf97fc8b3289e68439ad24967
GenySama <geenysong@gmail.com>
2020-01-03 19:18:11 +0800

    fix bugs

--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -8,14 +8,19 @@ Started 13/10/2015
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import StringProperty
 
 
 class SquareNumberApp(App):
     """Kivy App for squaring a number."""
+    status_message = StringProperty('Enter a number and press "Square"')
+
+    def __init__(self, **kwargs):
+        super(SquareNumberApp, self).__init__(**kwargs)
 
     def build(self):
         """Build the Kivy app from the kv file."""
-        Window.size = (200, 100)
+        Window.size = (500, 300)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
@@ -23,8 +28,11 @@ class SquareNumberApp(App):
     def handle_calculate(self, value):
         """Handle calculation (could be button press or other call),
         output result to label widget."""
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = float(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.status_message = "Not A Number"
 
 
 SquareNumberApp().run()
